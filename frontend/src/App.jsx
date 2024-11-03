fc5fa8aae865ec9225c10f0f8c79fdf1043e4d62
Saurabh Singh <singhsaurabh1606@gmail.com>
2024-11-03 15:53:23 +0530

    updating component file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import "./index.css";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <nav>Navbar</nav>
+      <Navbar />
       <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
         <Outlet />
       </main>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='max-w-screen-2xl mx-auto px-4 py-6'>
+       <nav>
+        {/* left side */}
+        <div>logo and search bar</div>
+
+        {/* center */}
+
+        {/* right side */}
+        <div>nav items</div>
+       </nav>
+    </header>
+  )
+}
+
+export default Navbar
